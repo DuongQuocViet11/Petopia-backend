ec65cf061c256dca5948d3d9a5f8b7442ec2230e
DuongQuocViet11 <duongquocv@gmail.com>
2022-09-19 18:42:31 +0700

    update

---
+++ src/main/java/com/aptech/petopia_backend/PetopiaBackendApplication.java
@@ -0,0 +1,14 @@
+package com.aptech.petopia_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PetopiaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PetopiaBackendApplication.class, args);
+    }
+    //commit
+
+}
